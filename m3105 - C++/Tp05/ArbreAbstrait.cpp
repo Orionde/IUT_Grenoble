9597610767f653d883a41eab19513d96aa203958
jorane <joraneju@gmail.com>
2015-10-12 09:23:21 +0200

    avancée instpour

--- m3105 - C++/Tp05/ArbreAbstrait.cpp
+++ m3105 - C++/Tp05/ArbreAbstrait.cpp
@@ -12,13 +12,13 @@ NoeudSeqInst::NoeudSeqInst() : m_instructions() {
 }
 
 int NoeudSeqInst::executer() {
-  for (unsigned int i = 0; i < m_instructions.size(); i++)
-    m_instructions[i]->executer(); // on exécute chaque instruction de la séquence
-  return 0; // La valeur renvoyée ne représente rien !
+	for (unsigned int i = 0; i < m_instructions.size(); i++)
+		m_instructions[i]->executer(); // on exécute chaque instruction de la séquence
+	return 0; // La valeur renvoyée ne représente rien !
 }
 
 void NoeudSeqInst::ajoute(Noeud* instruction) {
-  if (instruction!=nullptr) m_instructions.push_back(instruction);
+	if (instruction!=nullptr) m_instructions.push_back(instruction);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -26,13 +26,13 @@ void NoeudSeqInst::ajoute(Noeud* instruction) {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudAffectation::NoeudAffectation(Noeud* variable, Noeud* expression)
-: m_variable(variable), m_expression(expression) {
-}
+	: m_variable(variable), m_expression(expression) {
+	}
 
 int NoeudAffectation::executer() {
-  int valeur = m_expression->executer(); // On exécute (évalue) l'expression
-  ((SymboleValue*) m_variable)->setValeur(valeur); // On affecte la variable
-  return 0; // La valeur renvoyée ne représente rien !
+	int valeur = m_expression->executer(); // On exécute (évalue) l'expression
+	((SymboleValue*) m_variable)->setValeur(valeur); // On affecte la variable
+	return 0; // La valeur renvoyée ne représente rien !
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -40,31 +40,31 @@ int NoeudAffectation::executer() {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudOperateurBinaire::NoeudOperateurBinaire(Symbole operateur, Noeud* operandeGauche, Noeud* operandeDroit)
-: m_operateur(operateur), m_operandeGauche(operandeGauche), m_operandeDroit(operandeDroit) {
-}
+	: m_operateur(operateur), m_operandeGauche(operandeGauche), m_operandeDroit(operandeDroit) {
+	}
 
 int NoeudOperateurBinaire::executer() {
-  int og, od, valeur;
-  if (m_operandeGauche != nullptr) og = m_operandeGauche->executer(); // On évalue l'opérande gauche
-  if (m_operandeDroit != nullptr) od = m_operandeDroit->executer(); // On évalue l'opérande droit
-  // Et on combine les deux opérandes en fonctions de l'opérateur
-  if (this->m_operateur == "+") valeur = (og + od);
-  else if (this->m_operateur == "-") valeur = (og - od);
-  else if (this->m_operateur == "*") valeur = (og * od);
-  else if (this->m_operateur == "==") valeur = (og == od);
-  else if (this->m_operateur == "!=") valeur = (og != od);
-  else if (this->m_operateur == "<") valeur = (og < od);
-  else if (this->m_operateur == ">") valeur = (og > od);
-  else if (this->m_operateur == "<=") valeur = (og <= od);
-  else if (this->m_operateur == ">=") valeur = (og >= od);
-  else if (this->m_operateur == "et") valeur = (og && od);
-  else if (this->m_operateur == "ou") valeur = (og || od);
-  else if (this->m_operateur == "non") valeur = (!og);
-  else if (this->m_operateur == "/") {
-    if (od == 0) throw DivParZeroException();
-    valeur = og / od;
-  }
-  return valeur; // On retourne la valeur calculée
+	int og, od, valeur;
+	if (m_operandeGauche != nullptr) og = m_operandeGauche->executer(); // On évalue l'opérande gauche
+	if (m_operandeDroit != nullptr) od = m_operandeDroit->executer(); // On évalue l'opérande droit
+	// Et on combine les deux opérandes en fonctions de l'opérateur
+	if (this->m_operateur == "+") valeur = (og + od);
+	else if (this->m_operateur == "-") valeur = (og - od);
+	else if (this->m_operateur == "*") valeur = (og * od);
+	else if (this->m_operateur == "==") valeur = (og == od);
+	else if (this->m_operateur == "!=") valeur = (og != od);
+	else if (this->m_operateur == "<") valeur = (og < od);
+	else if (this->m_operateur == ">") valeur = (og > od);
+	else if (this->m_operateur == "<=") valeur = (og <= od);
+	else if (this->m_operateur == ">=") valeur = (og >= od);
+	else if (this->m_operateur == "et") valeur = (og && od);
+	else if (this->m_operateur == "ou") valeur = (og || od);
+	else if (this->m_operateur == "non") valeur = (!og);
+	else if (this->m_operateur == "/") {
+		if (od == 0) throw DivParZeroException();
+		valeur = og / od;
+	}
+	return valeur; // On retourne la valeur calculée
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -72,12 +72,12 @@ int NoeudOperateurBinaire::executer() {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudInstSi::NoeudInstSi(Noeud* condition, Noeud* sequence)
-: m_condition(condition), m_sequence(sequence) {
-}
+	: m_condition(condition), m_sequence(sequence) {
+	}
 
 int NoeudInstSi::executer() {
-  if (m_condition->executer()) m_sequence->executer();
-  return 0; // La valeur renvoyée ne représente rien !
+	if (m_condition->executer()) m_sequence->executer();
+	return 0; // La valeur renvoyée ne représente rien !
 }
 
 
@@ -86,12 +86,12 @@ int NoeudInstSi::executer() {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudInstTantQue::NoeudInstTantQue(Noeud* condition, Noeud* sequence)
-: m_condition(condition), m_sequence(sequence) {
-}
+	: m_condition(condition), m_sequence(sequence) {
+	}
 
 int NoeudInstTantQue::executer() {
-    while(m_condition->executer())m_sequence->executer();
-  return 0; // La valeur renvoyée ne représente rien !
+	while(m_condition->executer())m_sequence->executer();
+	return 0; // La valeur renvoyée ne représente rien !
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -99,12 +99,12 @@ int NoeudInstTantQue::executer() {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudInstRepeter::NoeudInstRepeter(Noeud* condition, Noeud* sequence)
-: m_condition(condition), m_sequence(sequence) {
-}
+	: m_condition(condition), m_sequence(sequence) {
+	}
 
 int NoeudInstRepeter::executer() {
-  while (m_condition->executer()) m_sequence->executer();
-  return 0; // La valeur renvoyée ne représente rien !
+	while (m_condition->executer()) m_sequence->executer();
+	return 0; // La valeur renvoyée ne représente rien !
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -112,16 +112,24 @@ int NoeudInstRepeter::executer() {
 ////////////////////////////////////////////////////////////////////////////////
 
 NoeudInstPour::NoeudInstPour(Noeud* condition, Noeud* seq, Noeud* aff1, Noeud* aff2)
-: m_condition(condition), m_sequence(seq), m_affectation(aff1), m_affectation1(aff2) {
-}
+	: m_condition(condition), m_sequence(seq), m_affectation(aff1), m_affectation1(aff2) {
+	}
 
 int NoeudInstPour::executer() {
-    if(m_affectation != nullptr && m_affectation1 != nullptr)
-    {
-        m_affectation->executer();
-        m_affectation1->executer();
-    }
-    while(m_condition->executer())
-    m_sequence->executer();
-  return 0; // La valeur renvoyée ne représente rien !
+	cout << m_affectation << endl;
+	cout << m_affectation1 << endl;
+	cout << m_condition << endl;
+	cout << m_sequence << endl;
+	if(m_affectation != nullptr && m_affectation1 != nullptr)
+	{
+		m_affectation->executer();
+		while(m_condition->executer())
+		{
+			m_affectation1->executer();
+			//m_sequence->executer();
+		}
+	}
+	/*while(m_condition->executer())
+	  m_sequence->executer();*/
+	return 0; // La valeur renvoyée ne représente rien !
 }
